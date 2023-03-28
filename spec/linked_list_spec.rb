36d7e603c9420f542de1a5c751f25f1f63720595
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-27 22:52:11 -0600

    Test: #append

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe LinkedList do
       expect(@list.head).to eq(nil)
     end
   end
+
+  describe '#append' do 
+    it 'appends a node' do
+      @list.append('West')
+      expect(@list.head).to be_a Node
+      expect(@list.head.surname).to eq('West')
+      expect(@list.head.next_node).to eq(nil)
+    end
+  end
 end
