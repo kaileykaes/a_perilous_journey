7074891aca1a567371392ca8934fb57f08e6a22f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-27 22:52:23 -0600

    Feat: #append

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,4 +4,15 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(data)
+    if @head == nil
+      @head = Node.new(data)      
+    else @head = present_node
+      until present_node.next_node == nil
+        present_node = present_node.next_node 
+      end
+      present_node.next_node = Node.new(data)
+    end
+  end
 end
