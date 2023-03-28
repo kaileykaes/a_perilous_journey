d151787455972cb5bbadcb293aa82770dc9f3e85
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-27 22:58:21 -0600

    Feat: #count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,11 +8,26 @@ class LinkedList
   def append(data)
     if @head == nil
       @head = Node.new(data)      
-    else @head = present_node
+    else present_node = @head 
       until present_node.next_node == nil
         present_node = present_node.next_node 
       end
       present_node.next_node = Node.new(data)
     end
+  end
+
+  def count 
+    counter = 0
+    if @head == nil 
+      0 
+    else
+      present_node = @head
+      counter += 1 
+      until present_node.next_node == nil
+        counter += 1 
+        present_node = present_node.next_node 
+      end
+    end
+    counter
   end
 end
